9e4853f6334ee941b6a575bdfb930837bdf4db0b
ShlokD <shlokpdesai@gmail.com>
2019-04-06 13:58:17 -0700

    build again

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js', { scope: './'}).then(reg => console.log('reg', reg))
+  navigator.serviceWorker.register('./sw.js', { scope: 'https://shlokd.github.io/fylo-landing-page/'}).then(reg => console.log('reg', reg))
   .catch(e => console.log('error registering service worker', e))
 }
